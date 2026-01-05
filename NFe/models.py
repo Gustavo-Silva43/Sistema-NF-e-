f64e98ff8acce10fcbea302168df91fbd74e41ef
Gustavo Silva <114096444+Gustavo-Silva43@users.noreply.github.com>
2026-01-05 15:17:45 -0300

    Correct syntax and comments in models.py
    Fix syntax error in Emitente model and update comments.

--- NFe/models.py
+++ NFe/models.py
@@ -23,7 +23,7 @@ class Emitente(models.Model):
     municipio = models.CharField(max_length=100)
     uf = models.CharField(max_length=2)
     cep = models.CharField(max_length=8)
-    telefone - models.CharField(max_length=20, blank=True)
+    telefone = models.CharField(max_length=20, blank=True)
 
     def __str__(self):
         returnself.razao_social
@@ -275,4 +275,5 @@ class Duplicata(models.Model):
 
 # valor = models.DecimalField("Valor", max_digits=12, decimal_places=2) O valor financeiro daquela parcela específica.
 # max_digits=12: Permite valores até 999.999.999,99.
+
 #decimal_places=2: Garante a precisão de duas casas decimais para os centavos.
