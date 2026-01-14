ac6b9d7272c04e1294e754dc7ef097190630fa1d
Gustavo Silva <114096444+Gustavo-Silva43@users.noreply.github.com>
2026-01-14 16:43:52 -0300

    Refactor NFe model and related classes

--- NFe/models.py
+++ NFe/models.py
@@ -151,7 +151,7 @@ class Produto(models.Model):
     tipi = models.CharField("TIPI", max_length=3, blank=True, null=True)
     cfop = models.CharField("CFOP", max_length=4)
     unidade = models.CharField("Unidade", max_length=6)
-    quantidade = models.DecimalField("Qtd", max_length=6, decimal_places=4)
+    quantidade = models.DecimalField("Qtd", mmax_digits=15, decimal_places=4)
     valor_unitario = models.DecimalField("Vr. Untário", max_digits=15)
     valor_total_bruto = models.DecimalField("Vr. Total Bruto", max_digits=15, decimal_places=2)
 
@@ -175,7 +175,7 @@ class Produto(models.Model):
     icms_percentual = models.CharField("% ICMS", max_digits=5, decimal_places=2, default=0)
     icms_valor = models.DecimalField("Vr. ICMS", max_digits=15, decimal_places=2, default=0)
 
-    icms_st_bc = models.DecimalField("BC CST (ST)", max_length=15, decimal_places=2, default=0)
+    icms_st_bc = models.DecimalField("BC CST (ST)", max_digits=15, decimal_places=2, default=0)
     icms_st = models.DecimalField("ICMS ST", max_length=15, decimal_places=2, default=0)
     icms_st_red_bc = models.DecimalField("Red.BC ICMS ST", max_digits=5, decimal_places=2, default=0)
     icms_st_valor = models.DecimalField("Vr. ICMS ST", max_digits=15, decimal_places=2, default=0)
@@ -191,8 +191,40 @@ class Produto(models.Model):
     cofins_bc = models.DecimalField("CONFINS BC", max_digits=15, decimal_places=2, default=0)
 
     cofins_percentual = models.DecimalField("%CONFINS", max_digits=5, decimal_places=2, default=0)
+    icms_uf_dest_bc = models.DecimalField("ICMSUFDest_vBCUFDest",max_digits=15, decimal_places=2, default=0)
+    icms_uf_dest_p_fcp = models.DecimalField("ICMSUFDest_pFCPUFDest", max_digits=5, decimal_places=2, default=0)
+    icms_uf_dest_p_inter = models.DecimalField("ICMSUFDest_pICMSInter", max_length=5, decimal_places=2, default=0)
+    icms_uf_dest_p_inter_part = models.DecimalField("ICMSUFDest_pICMSInterPart", max_length=5, decimal_places=2, default=0)
+    icms_uf_dest_v_fcp = models.DecimalField("ICMSUFDest_vFCPUFDest", max_digits=15, decimal_places=2, default=0)
 
-class NFe(models.Model)
+    icms_uf_dest_v_icms = models.DecimalField("ICMSUFDest_vICMSUFDest", max_digits=15, decimal_places=2, default=0)
+    icms_uf_remet_v_icms = models.DecimalField("ICMSUFDest_vICMSUFRemet", max_digits=15, decimal_places=2, default=0)
+
+    di_numero = models.CharField("Nº DI", max_length=12, blank=True, null=True)
+    di_data = models.DateField("Data DI", blank=True, null=True)
+    di_local_desemb = models.CharField("Local Desemb.", max_length=60, blank=True)
+    di_uf_desemb = models.CharField("UF Desemb.", max_length=2, blank=True)
+    di_data_desemb = models.CharField("Data Desemb.", blank=True, null=True)
+    di_via_transporte = models.CharField("Via Transporte", max_length=2, blank=True, null=True)
+    afrmm = models.DecimalField("AFRMM", max_digits=15, decimal_places=2, default=0)
+    forma_importacao = models.CharField("Forma Importação", max_length=1, blank=True, null=True)
+    importa_cnpj = models.CharField("CNPJ", max_length=14, blank=True)
+    importa_uf_terceiro = models.CharField("UF TErceiro", max_length=14, blank=True, null=True)
+    cod_exportador = models.CharField("Cód. Exportador", max_length=60, blank=True, null=True)
+    n_adicao = models.CharField("nAdicao", blank=True, null=True)
+    n_seq_adic = models.IntegerField("nSeqAdic", blank=True, null=True)
+    c_fabricante = models.CharField("cFabricante", max_length=60, blank=True)
+    v_desc_di = models.DecimalField("vDescDI", max_digits=15, decimal_places=2, default=0)
+
+    ipi_cst = models.CharField("IPI_CST", max_length=2, blank=True, null=True)
+    ipi_bc = models.DecimalField("IPI_vBC", max_digits=15, decimal_places=2, default=0)
+    ipi_percentual = models.DecimalField("IPI_pIPI", max_digits=5, decimal_places=2, default=0)
+    ipi_valor = models.DecimalField("IPI_vIPI", max_digits=15, decimal_places=2, default=0)
+
+    def __str__(self):
+        return f"{self.codigo_produto} - {self.descricao}"
+
+class NFe(models.Model):
     MODELO_CHOICES = [('55', '55 - NF-e'), ('65', '65 - NFC-e')]
     TP_EMIS_CHOICES = [('1', 'Normal'), ('9', 'Contingência')]
     TP_NF_CHOICES = [('0', 'Entrada'), ('1', 'Saída')]
@@ -289,10 +321,40 @@ class Volume(models.Model):
     peso_bruto = models.DecimalField("Peso Bruto", max_digits=10, decimal_places=3)
 
 
-class Duplicata(models.Model):
-    nfe = models.ForeignKey(NFe, on_delete=models.CASCADE, related_name="duplicatas")
-    numero = models.CharField("N° Duplicata", max_length=20)
-    data_vencimento = models.DateField("Data Vencimento")
-    valor = models.DecimalField("Valor", max_digits=12, decimal_places=2)
+class Pagamento(models.Model):
+    IND_PAGTO_CHOICES = [
+        ('0', '0 - Pagamento á Vista'),
+        ('1', '1 - Pagamento á Prazo'),
+    ]
+
+    FORMA_PAGTO_CHOICES = [
+        ('01', '01 - Dinheiro')
+        ('02', '02 - Cheque'),
+        ('03', '03 - Cartão de Credito'),
+        ('04', '04 - Cartão de Débito'),
+        ('15', '15 - Boleto Bancário'),
+        ('90', '90 - Sem pagamento'),
+        ('99', '99 - Outros'),
+    ]
+
+    nfe = models.ForeignKey('NFe', related_name='pagamentos', on_delete=models.CASCADE)
+
+    numero_forma = models.IntegerField("Nº Forma")
+    indicador_pagamento = models.CharField("Ind. Forma Pagto.", max_length=1, choices=IND_PAGTO_CHOICES)
+    forma_pagamento = models.CharField("Forma Pagto",max_length=2, choices=FORMA_PAGTO_CHOICES)
+    valor_pagamento = models.DecimalField("Valor Pagto", max_digits=15, decimal_places=2)
+
+    tipo_integracao = models.CharField("Tipo Integração CC", max_length=1, blank=True, null=True)
+    cnpj_credenciadora = models.CharField("CNPJ Credenciadora CC",max_length=14, blank=True, null=True)
+    bandeira_cc = models.CHarField("Bandeira CC", max_length=2, blank=True, null=True)
+    autorizacao_transacao = models.CharField("Nº Autorização Transação",max_length=20, blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Pagamento da NF-e"
+        verbose_name_plural = "Pagamento da NF-e"
+
+        def __str__(self):
+            return f"{self.numero_forma} - {self.get_forma_pagamento_display()} - R$ {self.valor_pagamento}"
+
 
 
