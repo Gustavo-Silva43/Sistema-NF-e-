2ebb4abbf12d07c5dfbce8fecd1ddcedd78c0aa6
Gustavo Silva <114096444+Gustavo-Silva43@users.noreply.github.com>
2026-01-16 17:48:21 -0300

    Update URL patterns for NFe views

--- NFe/urls.py
+++ NFe/urls.py
@@ -1,7 +1,5 @@
-from django.urls import path
-from . import views
-
 urlpatterns = [
     path('', views.emitir_nfe, name='emitir_nfe'),
-    path('<int:pk>/', views.emitir_nfe, name='emitir_nfe'),    
+    path('<int:pk>/', views.gerenciar_nfe, name='gerenciar_nfe'),   
+    path('emissao/', views.emitir_nfe, name='emissao_nfe'), 
 ]
