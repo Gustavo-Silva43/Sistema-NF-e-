abdaadb316625af3bfdef68f706510cf3eb2407c
Gustavo Silva <114096444+Gustavo-Silva43@users.noreply.github.com>
2026-03-19 17:20:14 -0300

    Update urls.py

--- NFe/urls.py
+++ NFe/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.emitir_nfe, name='emitir_nfe'),
     path('<int:pk>/', views.gerenciar_nfe, name='gerenciar_nfe'),   
     path('emissao/', views.emitir_nfe, name='emissao_nfe'), 
+    path('arquivos/<int:nfe_id>/', views.abrir_pasta_arquivos, name='abrir_pasta_arquivos'),
 ]
